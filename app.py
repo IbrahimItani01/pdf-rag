e031136004e16f57e751ad1428e2cdde8b531aac
IbrahimItani <i.itani2002@gmail.com>
2025-08-29 23:44:10 +0300

    app<>updated the app file to be more robust for the uplaod api

--- app.py
+++ app.py
@@ -1,14 +1,15 @@
-from fastapi import FastAPI
-from src.shared.constants import version
+from fastapi import FastAPI, Depends
+from src.shared.constants import version,title,description
 from src.services.upload_file_service import process_pdf_file
 from src.models.upload_file_models import UploadFileRequest
+from src.middlewares.verify_api_key import verify_api_key
 
-app = FastAPI()
+app = FastAPI(version=version,title=title,description=description)
 
 @app.get("/health")
 async def root():
     return {"message": "Server running","version":version}
 
-@app.post("/upload")
+@app.post("/upload",dependencies==[Depends(verify_api_key)])
 async def upload_file(request: UploadFileRequest):
     return process_pdf_file()
