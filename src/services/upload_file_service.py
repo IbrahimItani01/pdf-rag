ffce05d0759e874f2d6a01a602080371ea94839f
IbrahimItani <i.itani2002@gmail.com>
2025-08-30 15:43:18 +0300

    services<>updated the process pdf file service function to return the token count of the pdf file using openai tokenizer package

--- src/services/upload_file_service.py
+++ src/services/upload_file_service.py
@@ -1,2 +1,21 @@
-def process_pdf_file():
-    return "Hey"
+import os
+import tempfile
+from fastapi import UploadFile, Request
+from langchain_community.document_loaders import PyPDFLoader
+from src.shared.constants import supported_model
+from src.shared.utils import tokenize_document
+
+def process_pdf_file(request: Request,file: UploadFile):
+    with tempfile.NamedTemporaryFile(delete=False, suffix=".pdf") as temp_file:
+        temp_file.write(file.file.read())
+        temp_file_path = temp_file.name
+    try:
+        loader = PyPDFLoader(temp_file_path)
+        documents = loader.load()
+        total_pages = documents[0].metadata['total_pages']
+        token_count = tokenize_document(documents)
+
+        return {"message": "PDF processed successfully", "metadata":{"pages": total_pages, "token_count": token_count} ,"version": request.app.version }
+    
+    finally:
+        os.unlink(temp_file_path)
--- src/shared/constants.py
+++ src/shared/constants.py
@@ -6,3 +6,5 @@ description = "API for PDF RAG"
 # Upload API Related
 allowed_file_extensions = {".pdf"}
 allowed_file_size = 2 * 1024 * 1024 # 2MB
+
+supported_model= "gpt-5-nano"
