fb587df6bc7bd7a3226da658321941d5e558dbe4
IbrahimItani <i.itani2002@gmail.com>
2025-08-30 22:16:45 +0300

    models<>added keys to the upload api response

--- src/models/responses.py
+++ src/models/responses.py
@@ -3,6 +3,8 @@ from pydantic import BaseModel
 class ProcessingMetadata(BaseModel):
     pages: int
     token_count: int
+    chunks_stored: int
+    file_id: str
 
 class UploadFileResponse(BaseModel):
     message: str
