53662cebf96118419bedecb92ae88179395712d6
IbrahimItani <i.itani2002@gmail.com>
2025-08-31 12:33:10 +0300

    bugs<>fixed bugs in route and service related to user registeration

--- src/routes/users_route.py
+++ src/routes/users_route.py
@@ -2,6 +2,7 @@ from fastapi import APIRouter, Request, Depends
 from src.models.requests import UserRegisterRequest
 from src.services.users_services import register_user
 from src.middlewares.verify_api_key import verify_api_key
+from src.middlewares.validate_user_data import validate_user_register_data
 from src.models.responses import UserRegisterResponse
 
 router = APIRouter(
@@ -10,10 +11,9 @@ router = APIRouter(
     dependencies=[Depends(verify_api_key)]  
 )
 
-@router.post("/register", response_model=UserRegisterRequest)
+@router.post("/register", response_model=UserRegisterResponse)
 async def upload_file(
     request: Request,
-    user_data: UserRegisterRequest,
-    
+    user_data: UserRegisterRequest = Depends(validate_user_register_data),
 ):
     return register_user(request,user_data)
--- src/services/users_services.py
+++ src/services/users_services.py
@@ -1,9 +1,9 @@
 from fastapi import Request
 from src.models.requests import UserRegisterRequest
 from src.models.responses import UserRegisterResponse
-def register_user(request:Request,user:UserRegisterRequest) -> UserRegisterResponse:
+def register_user(request: Request, user: UserRegisterRequest) -> UserRegisterResponse:
     return {
-        message: "User registered successfully",
-        user_token: "12313",
-        version: request.app.version
+        "message": "User registered successfully",
+        "user_token": "12313",
+        "version": request.app.version
     }
