28dd28568f2f577e7421fa0585323a124c99b832
IbrahimItani <i.itani2002@gmail.com>
2025-08-31 14:36:18 +0300

    routes<>created a user login route

--- src/routes/users_route.py
+++ src/routes/users_route.py
@@ -1,9 +1,9 @@
 from fastapi import APIRouter, Request, Depends
-from src.models.requests import UserRegisterRequest
-from src.services.users_services import register_user
+from src.models.requests import UserRegisterRequest,UserLoginRequest
+from src.services.users_services import register_user,login_user
 from src.middlewares.verify_api_key import verify_api_key
 from src.middlewares.validate_user_data import validate_user_register_data
-from src.models.responses import UserRegisterResponse
+from src.models.responses import UserRegisterResponse, UserLoginResponse
 
 router = APIRouter(
     prefix="/users",       
@@ -17,3 +17,10 @@ async def upload_file(
     user_data: UserRegisterRequest = Depends(validate_user_register_data),
 ):
     return register_user(request,user_data)
+
+@router.post("/login", response_model=UserLoginResponse)
+async def upload_file(
+    request: Request,
+    user_data: UserLoginRequest,
+):
+    return login_user(request,user_data)
