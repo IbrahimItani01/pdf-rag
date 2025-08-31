b9ae9a57269e72b6683969357c7a2cb90fc667cb
IbrahimItani <i.itani2002@gmail.com>
2025-08-31 12:01:35 +0300

    app<>inlcuded the user register api

--- app.py
+++ app.py
@@ -2,10 +2,11 @@ from dotenv import load_dotenv
 load_dotenv()
 from fastapi import FastAPI
 from src.shared.constants import version, title, description
-from src.routes import files_route,health_route
+from src.routes import files_route,health_route,users_route
 
 
 app = FastAPI(version=version, title=title, description=description)
 
 app.include_router(health_route.router)
 app.include_router(files_route.router)
+app.include_router(users_route)
