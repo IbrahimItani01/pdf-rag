3d3b97b2875e96c03d30815c7af777183a3d145f
IbrahimItani <i.itani2002@gmail.com>
2025-09-01 22:09:26 +0300

    services<>reformed how openai client is returned

--- src/services/gateway_services.py
+++ src/services/gateway_services.py
@@ -1,9 +1,20 @@
 
 from openai import OpenAI
 from pinecone import Pinecone, ServerlessSpec
-from src.shared.utils import get_env_variable
+from cryptography.fernet import Fernet
+from src.shared.env import get_env_variable
 from supabase import create_client, Client
 
 pc_client = Pinecone(api_key=get_env_variable("PINECONE_API_KEY"))
-openai_client = OpenAI(api_key=get_env_variable("OPEN_AI_API_KEY"))
 supabase_client: Client = create_client(get_env_variable("SUPABASE_URL"), get_env_variable("SUPABASE_KEY"))
+fernet = Fernet(get_env_variable("FERNET_ENCRYPTION_KEY"))
+
+def return_openai_client(api_key: str | None = None):
+    if api_key is None:
+        print("Using default OpenAI API key")
+        openai_client = OpenAI(api_key=get_env_variable("OPEN_AI_API_KEY"))
+        return openai_client
+    else:
+        print("Using provided OpenAI API key")
+        openai_client = OpenAI(api_key=api_key)
+        return openai_client
